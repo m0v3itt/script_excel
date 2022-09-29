0a30b84ab33af17c0b0914fea2abb9baebb1a4f6
m0v3itt <63509746+m0v3itt@users.noreply.github.com>
2022-09-29 16:56:19 +0100

    Add files via upload

--- export.php
+++ export.php
@@ -1,279 +1,146 @@
-<?php
-use Phppot\DataSource;
-use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
-
-require_once 'DataSource.php';
-$db = new DataSource();
-$conn = $db->getConnection();
-require_once ('./vendor/autoload.php');
-
-if (isset($_POST["import"])) {
-
-    $allowedFileType = [
-        'application/vnd.ms-excel',
-        'text/xls',
-        'text/xlsx',
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    ];
-
-    if (in_array($_FILES["file"]["type"], $allowedFileType)) {
-
-        $targetPath = 'uploads/' . $_FILES['file']['name'];
-        move_uploaded_file($_FILES['file']['tmp_name'], $targetPath);
-
-        $Reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-
-        $spreadSheet = $Reader->load($targetPath);
-        $excelSheet = $spreadSheet->getActiveSheet();
-        $spreadSheetAry = $excelSheet->toArray();
-        $sheetCount = count($spreadSheetAry);
-
-        for ($i = 2; $i <= $sheetCount; $i ++) {
-            $dias = "";
-            if (isset($spreadSheetAry[0][$i])) {
-                $dias = mysqli_real_escape_string($conn, $spreadSheetAry[0][$i]);
-            }
-        
-            
-
-            if (! empty($dias)) {
-                $query = "insert into tb_dias(dia) values(?)";
-                $paramType = "s";
-                $paramArray = array(
-                    $dias
-                );
-                $insertId = $db->insert($query, $paramType, $paramArray);
-                if (! empty($insertId)) {
-                    $type = "success";
-                    $message = "Excel Data Imported into the Database";
-                } else {
-                    $type = "error";
-                    $message = "Problem in Importing Excel Data!!!!!!!!!";
-                }
-            }
-        }
-    } else {
-        $type = "error";
-        $message = "Invalid File Type. Upload Excel File.";
-    }
-
-    for ($i = 1; $i <= $sheetCount; $i ++) {
-        $name = "";
-        if (isset($spreadSheetAry[$i][0])) {
-            $name = mysqli_real_escape_string($conn, $spreadSheetAry[$i][0]);
-            preg_match('/\(([A-Za-z0-9 ]+?)\)/', $name, $out);
-            $codigo=$out[1];
-     
-        }
-        $preferencias = "";
-        if (isset($spreadSheetAry[$i][1])) {
-            $preferencias = mysqli_real_escape_string($conn, $spreadSheetAry[$i][1]);
-        }
-        
-
-        if (! empty($name) || ! empty($preferencias)) {
-            $query = "insert into tb_nadadores(nome,preferencias,id_nadador) values(?,?,?)";
-            $paramType = "ssi";
-            $paramArray = array(
-                $name,
-                $preferencias,
-                $codigo
-            );
-            $insertId = $db->insert($query, $paramType, $paramArray);
-            if (! empty($insertId)) {
-                $type = "success";
-                $message = "Excel Data Imported into the Database";
-            } else {
-                $type = "error";
-                $message = "Problem in Importing Excel Data.............";
-            }
-        }
-    }
-    
-    
-}
-
-
-
-
-        
-
+<?php 
+include_once("db_connect.php");
 
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <head>
-    <style>
-    body {
-        font-family: Arial;
-        width: 550px;
-    }
-
-    .outer-container {
-        background: #F0F0F0;
-        border: #e0dfdf 1px solid;
-        padding: 40px 20px;
-        border-radius: 2px;
-    }
-
-    .btn-submit {
-        background: #333;
-        border: #1d1d1d 1px solid;
-        border-radius: 2px;
-        color: #f0f0f0;
-        cursor: pointer;
-        padding: 5px 20px;
-        font-size: 0.9em;
-    }
-
-    .tutorial-table {
-        margin-top: 40px;
-        font-size: 0.8em;
-        border-collapse: collapse;
-        width: 100%;
-    }
-
-    .tutorial-table th {
-        background: #f0f0f0;
-        border-bottom: 1px solid #dddddd;
-        padding: 8px;
-        text-align: left;
-    }
-
-    .tutorial-table td {
-        background: #FFF;
-        border-bottom: 1px solid #dddddd;
-        padding: 8px;
-        text-align: left;
-    }
-
-    #response {
-        padding: 10px;
-        margin-top: 10px;
-        border-radius: 2px;
-        display: none;
-    }
-
-    .success {
-        background: #c7efd9;
-        border: #bbe2cd 1px solid;
-    }
-
-    .error {
-        background: #fbcfcf;
-        border: #f3c6c7 1px solid;
-    }
-
-    div#response.display-block {
-        display: block;
-    }
-
-</style>
-
+<title>Script</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- jQuery -->
+<script type="text/javascript" src="dist/jquery.tabledit.js"></script>
 </head>
+<body class="">
 
-<body>
-    <h2>Import Excel File into MySQL Database using PHP</h2>
+	<div class="container" style="min-height:500px;">
+	<div class=''>
+	</div>
 
-    <div class="outer-container">
-        <form action="" method="post" name="range" enctype="multipart/form-data">
-            <div>
-            <?php
-     $sqlSelect = "SELECT * FROM tb_dias";
-     $resultad = $db->select($sqlSelect);
-     echo"<select name='Dias' size='1' class='form-select form-select-sm'>";
-     echo"<option value='' disabled selected hidden> Dias </option>"; 
-                            foreach($resultad as $re){
-                                $dia1=$re['dia']; 
-                                $id1=$re['id_dia'];
-                                echo"<option value=$id1>$dia1</option>";     
-                                } 
-                                echo "</select>"; 
-                                echo"<select name='Dias' size='1' class='form-select form-select-sm'>";
-     echo"<option value='' disabled selected hidden> Dias </option>"; 
-                            foreach($resultad as $re){
-                                $dia2=$re['dia']; 
-                                $id2=$re['id_dia'];
-                                echo"<option value=$id2>$dia2</option>";     
-                                } 
-                                echo "</select>";
+<?php 
 
-                                echo "<input type='submit'>"
 
+
+// Selecionar os dias que vão aparecer na tabela
+
+
+if (isset($_POST["enviar"])) {
+    $um=$_POST['dia1'];
+	$dois=$_POST['dia2'];
+		$sql_query =  "SELECT * from tb_dias where id_dia between $um and $dois";
+	   	$resultsett = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));
+    
+}
 ?>
-                
-            </div>
+<div class="container home">	
+	
+	<form  method="post" name="rangee">
+       <label> Insira as datas </label>
+       
+       <?php
+       
+       // escolher os dias que vão aparecer na tabela
+	   $sql_query = "SELECT * FROM tb_dias";
+	   $resultset = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));
+       echo"<select name='dia1' size='1' class='form-select form-select-sm'>";
+       echo"<option value='' disabled selected hidden> Dias </option>"; 
+	   while( $re = mysqli_fetch_assoc($resultset) ) {
+                                  $dia1=$re['dia']; 
+                                  $id1=$re['id_dia'];
+                                  echo"<option value=$id1>$dia1</option>";     
+                                  } 
+      ?> 
+        </select>
 
-        </form>
-
-    </div>
-    <div id="response" class="<?php if(!empty($type)) { echo $type . " display-block"; } ?>">
-        <?php if(!empty($message)) { echo $message; } ?></div>
-
-    <table class='tutorial-table'>
-        <thead>
-            <tr>
-           
-                <th>Nome_praia</th>
-                <th>Turno_praia</th>
-                <?php
-                if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                
-                $sqlSelect = "SELECT * FROM tb_dias where dia between $id1 and $id2";
-                    $result = $db->select($sqlSelect);
-                    var_dump($result);
-                    
-                    if (! empty($result)) {
-                        foreach ($result as $row) { 
-                            echo "<th>".$row['dia']."</th>";
-                        }}
-                    ?>
-            
-
-            </tr>
-        </thead>
         <?php
-        $sqlSelect = "SELECT * FROM tb_praia";
-        $resultado = $db->select($sqlSelect);
-        
-    foreach ($resultado as $res) { // ($row = mysqli_fetch_array($result))
-        ?>
-        <tbody>
-            <tr><?php 
-          
-           
-                 echo '<td>' .$res['nome_praia']. '</td>';
-                 echo '<td>'  .$res['turno']. '</td>';
+      $sql_query = "SELECT * FROM tb_dias";
+	  $resultset = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));
+       echo"<select name='dia2' size='1' class='form-select form-select-sm'>";
+       echo"<option value='' disabled selected hidden> Dias </option>"; 
+	   while( $re = mysqli_fetch_assoc($resultset) ) {
+                                  $dia2=$re['dia']; 
+                                  $id2=$re['id_dia'];
+                                  echo"<option value=$id2>$dia2</option>";     
+                                  } 
+      ?> 
+        </select>
+         <input type="submit" name="enviar" class="alertButton">                         
+    </form>	 
+	
+	<table id="data_table" class="table table-dark">
+		<thead>
+			<tr>
+				<th>Id</th>
+				<th>Nome Praia</th>
+				<th>Turno</th>
+				<?php 
+				$x=0;
+				while( $row = mysqli_fetch_assoc($resultsett) ) {
+					 
+					echo "<th>".$row['dia']."</th>";
+                            $id_dia=$row['id_dia'];
+							$x++;
+				}
+						
+				?>
+				
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+			$sql_query = "SELECT * FROM tb_praia";
+			$resultset = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));
+			while( $developer = mysqli_fetch_assoc($resultset) ) {
+			?>
+			   <tr id="<?php echo $developer ['id_praia']; ?>">
+			   <td><?php echo $developer ['id_praia']; ?></td>
+			   <td><?php echo $developer ['nome_praia']; ?></td>
+			   <td><?php echo $developer ['turno']; ?></td>
+			   <?php for($i=0;$i<$x;$i++){
+				echo "<td>".' '."</td>";
+			   }
+			   ?>
 
-                ?>
+			</tr>
+			<?php } ?>
 
-            </tr>
-            <?php
-    }
-    ?>
-        </tbody>
-    </table>
-    <?php
-     $sqlSelect = "SELECT dia FROM tb_dias";
-     $resultad = $db->select($sqlSelect);
-     echo"<select name='Dias' size='1' class='form-select form-select-sm'>";
-     echo"<option value='' disabled selected hidden> Dias </option>"; 
-                            foreach($resultad as $re){
-                                $dias=$re['dia']; 
-                                echo"<option value='$dias'>$dias</option>";     
-                                } 
-                                echo "</select>";  
-                            }
-?>
-    <script>
-    function resetFile() {
-        const file = document.querySelector('.file');
-        file.value = '';
-    }
-    </script>
+		</tbody>
+    </table>	
+    <div style="margin:50px 0px 0px 0px;">
+    <button id="download-button">Download CSV</button> 
+	</div>                       
+    
+	
+	
+</div>
+<script>
+	$('.table').onload(function()
+{
+                                                  
+    var selectedElements = $('th');
+	for(int i=2; i<selectedElements.length; i++){
+		console.log(i)
+	}  
+                      
+});
+
+</script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>          
+<script type="text/javascript" src="js/custom_table_edit.js"></script>
+<script type="text/javascript" src="js/export_csv.js"></script>
+<div class="insert-post-ads1" style="margin-top:20px;">
+
+</div>
+</div>
 </body>
-
 </html>
+ 
 
+
+
+                                                                                                       
